185b0639cfa4a4993252c54fa87d009a12ae2888
mwakulinski <michal.wakulinski1@gmail.com>
2022-03-28 12:56:59 +0200

    add UserType

--- pages/api/search.tsx
+++ pages/api/search.tsx
@@ -1,14 +1,14 @@
 import { RepositoryType } from "../../src/types/RepositoryType";
+import { UserType } from "../../src/types/UserType";
 import type { NextApiRequest, NextApiResponse } from "next";
 const axios = require("axios").default;
 
-const getRepos = async (
-  req: NextApiRequest,
-  res: NextApiResponse<RepositoryType>
-) => {
+const getRepos = async (req: NextApiRequest, res: NextApiResponse<any>) => {
   const repositories = await axios.get("https://api.github.com/repositories");
-  const data = repositories.data;
-  res.status(200).json();
+  const dataRepo = repositories.data as RepositoryType[];
+  const users = await axios.get("https://api.github.com/users");
+  const dataUser = users.data as UserType[];
+  const response = [...dataUser, ...dataRepo];
+  res.status(200).json(response);
 };
-
 export default getRepos;
