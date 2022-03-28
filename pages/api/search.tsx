000bd3c657cac8f94ed3f20d7d38eddd676213dd
mwakulinski <michal.wakulinski1@gmail.com>
2022-03-28 15:01:43 +0200

    implemetn fetching repos in proper format

--- pages/api/search.tsx
+++ pages/api/search.tsx
@@ -1,33 +1,59 @@
-import { RepositoryType } from "../../src/types/RepositoryType";
 import { UserType } from "../../src/types/UserType";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { User } from "../../interfaces/ResultInterFace";
+import { Repository, User } from "../../interfaces/ResultInterFace";
+import { RepositoryType } from "../../src/types/RepositoryType";
 const axios = require("axios").default;
 
 let token = process.env.MY_TOKEN;
 
+// async function getSearchUser(req: NextApiRequest) {
+//   const res = await fetch(
+//     `https://api.github.com/search/users?q=${req.query.search}`,
+//     {
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     }
+//   );
+//   const data = await res.json();
+//   return data.items;
+// }
+
 const getRepos = async (req: NextApiRequest, res: NextApiResponse<any>) => {
-  const repositories = await axios.get("https://api.github.com/repositories", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  const dataRepo = (await repositories.data) as RepositoryType[];
-  const users = await axios.get("https://api.github.com/users", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  const dataUsers = (await users.data) as UserType[];
-  const properUsers = dataUsers.map((dataUser) => {
+  const repositories = await axios.get(
+    `https://api.github.com/search/repositories?q=${req.query.search}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const dataRepos = (await repositories.data.items) as RepositoryType[];
+  const properRepos = dataRepos.map((dataRepo) => {
     return {
-      name: dataUser.login,
-      avatarUrl: dataUser.avatar_url,
-      description: dataUser.events_url,
-      place: dataUser.node_id,
-    } as User;
+      name: dataRepo.name,
+      description: dataRepo.description,
+      mainLanguage: dataRepo.language,
+      observers: dataRepo.watchers_count,
+      lastUpdate: dataRepo.updated_at,
+    } as Repository;
   });
-  const response = [...properUsers, ...dataRepo];
-  res.status(200).json(response);
+
+  // const users = await axios.get("https://api.github.com/search/users", {
+  //   headers: {
+  //     Authorization: `Bearer ${token}`,
+  //   },
+  // });
+  // const dataUsers = (await users.data) as UserType[];
+  // const properUsers = dataUsers.map((dataUser) => {
+  //   return {
+  //     name: dataUser.login,
+  //     avatarUrl: dataUser.avatar_url,
+  //     description: dataUser.events_url,
+  //     place: dataUser.node_id,
+  //   } as User;
+  // });
+  // const response = [...dataRepos];
+  res.status(200).json(properRepos);
 };
 export default getRepos;
