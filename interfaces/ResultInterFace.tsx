0c35c2c86dde26221031cb3d9fde0b8d58697b68
zuzanapiorkowska <napiorkowskaz@gmail.com>
2022-03-28 10:44:50 +0200

    add IResult Interface

---
+++ interfaces/ResultInterFace.tsx
@@ -0,0 +1,8 @@
+export interface IResult {
+    name: string;
+    description: string;
+    observers: number;
+    mainLanguage: string;
+    colorOfMainLanguagE: string;
+    lastUpdate: string;
+  }
